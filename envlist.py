0a951d1220d497f35c7221402d2c4173019151e5
Shenghao Chen <chen.shenghao@u.nus.edu>
2020-10-25 14:33:50 +0800

    Cast nenvs to int in envlist.py

--- envlist.py
+++ envlist.py
@@ -43,7 +43,7 @@ with lock:
         # create a list (named clist) of nevns environments with the 
         # prefix envprefix
         # add code here
-        clist = [envprefix + str(i) for i in range(nenvs)]
+        clist = [envprefix + str(i) for i in range(int(nenvs))]
     else:
         # load hickle file
         clist = hickle.load(file_path)
